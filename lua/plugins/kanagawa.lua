347e8a3646000e11f71547d4a4a9da647cd95c92
cy <cyrod@hey.com>
2025-06-06 09:54:04 +0800

    WIP

--- lua/plugins/kanagawa.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'rebelot/kanagawa.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  init = function()
-    vim.cmd.colorscheme 'kanagawa-wave'
-    vim.cmd.hi 'Comment gui=none'
-  end,
-}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Personal Neovim Config
+
+Inspired by Jess Archer, TJ Devries, typecraft, and Kickstart
+
+WIP...

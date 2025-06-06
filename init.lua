4edffeecda55a42060df122a42580c4309b7697f
cy <cyrod@hey.com>
2025-06-06 08:05:18 +0800

    Cleanup

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
--- NOTE: Here is where you install your plugins.
+require 'devloper'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-require 'devloper'
